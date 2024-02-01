47da98f0ce59dbdee46b577089307018be55f161
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-01 20:57:50 +0000

    finish challenge

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,23 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    range = ("A".."Z").to_a
+    10.times do
+      @letters << range.sample
+    end
+
+  end
+
+  def score
+    @word1 = params[:word].downcase
+    @word2 = params[:letters].downcase
+    @check1 = @word1.chars.all? { |letter| @word2.include?(letter) }
+
+    url = "https://wagon-dictionary.herokuapp.com/#{@word1}"
+    url_result = URI.open(url).read
+    @check2 = JSON.parse(url_result)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "games#new"
+  post "score", to: "games#score"
 end
